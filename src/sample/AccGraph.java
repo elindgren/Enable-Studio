5256334c49805e4b98a34f92d0b3f9fd3d3cb6dc
SpaceyCat <ericlin@student.chalmers.se>
2017-04-11 16:52:59 +0200

    Added component view of static data graph

--- src/sample/AccGraph.java
+++ src/sample/AccGraph.java
@@ -17,24 +17,21 @@ import javafx.stage.Stage;
 public class AccGraph extends Application {
 
     @Override public void start(Stage stage){
+        double[] data;
+
+
         stage.setTitle("JANA-viewer");
 
         //Creating 2 gridpanes, one for each scene
 
-        GridPane grid1 = new GridPane();
-        GridPane grid2 = new GridPane();
+        GridPane grid = new GridPane();
 
         //Setting alignements, gap between columns and padding before the borders for the grid
-        grid1.setAlignment(Pos.CENTER);
-        grid2.setAlignment(Pos.CENTER);
+        grid.setAlignment(Pos.CENTER);
 
-        grid1.setHgap(10);
-        grid1.setVgap(10);
-        grid1.setPadding(new Insets(25,25,25,25)); //The padding in TOP, RIGHT, BOTTOM, LEFT
-
-        grid2.setHgap(10);
-        grid2.setVgap(10);
-        grid2.setPadding(new Insets(25,25,25,25)); //The padding in TOP, RIGHT, BOTTOM, LEFT
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25)); //The padding in TOP, RIGHT, BOTTOM, LEFT
 
 
         //defining the axes
@@ -47,24 +44,39 @@ public class AccGraph extends Application {
         final NumberAxis dynyAxis = new NumberAxis();
         dynxAxis.setLabel("time, <s>");
         dynyAxis.setLabel("resulting acceleration, <m/s^2>");
-        //creating the chart
+
+        final NumberAxis statxyzxAxis = new NumberAxis();
+        final NumberAxis statxyzyAxis = new NumberAxis();
+        statxyzxAxis.setLabel("time, <s>");
+        statxyzyAxis.setLabel("resulting acceleration, <m/s^2>");
+
+        //creating the charts
         final LineChart<Number,Number> statChart =
                 new LineChart<Number,Number>(statxAxis,statyAxis);
 
         final LineChart<Number,Number> dynChart = new LineChart<Number,Number>(dynxAxis,dynyAxis);
 
+        final LineChart<Number,Number> statxyzChart = new LineChart<Number,Number>(statxyzxAxis,statxyzyAxis);
+
         statChart.setTitle("Magnitude of resulting acceleration");
         dynChart.setTitle("Magnitude of resulting acceleration");
+        statxyzChart.setTitle("Magnitude of resulting acceleration");
 
         //defining a static series
         XYChart.Series statSeries = new XYChart.Series();
-        statSeries.setName("Resulting acceleration, static");
+        statSeries.setName("Resulting acceleration, static, resultant");
         //defining a dynamic series
         XYChart.Series dynSeries = new XYChart.Series();
-        dynSeries.setName("Resulting acceleration, dynamic");
+        dynSeries.setName("Resulting acceleration, dynamic, resultant");
+        //defining statxyz-series
+        XYChart.Series xstatxyzSeries = new XYChart.Series();
+        xstatxyzSeries.setName("Acceleration, x");
+        XYChart.Series ystatxyzSeries = new XYChart.Series();
+        ystatxyzSeries.setName("Acceleration, y");
+        XYChart.Series zstatxyzSeries = new XYChart.Series();
+        zstatxyzSeries.setName("Acceleration, z");
 
         //populating the static series with data
-
         for(int i = 0; i <=10; i++) {
             statSeries.getData().add(new XYChart.Data(i,i));
         }
@@ -76,42 +88,65 @@ public class AccGraph extends Application {
         }
         dynChart.getData().add(dynSeries);
 
+        //populating the static xyz-series with data
+        for(int i = 10; i<20; i++ ){
+            xstatxyzSeries.getData().add(new XYChart.Data(i-2,i-2));
+            ystatxyzSeries.getData().add(new XYChart.Data(i-20,i-20));
+            zstatxyzSeries.getData().add(new XYChart.Data(i+5,i+5));
+        }
+        statxyzChart.getData().addAll(xstatxyzSeries,ystatxyzSeries,zstatxyzSeries);
 
         //Setting dots to false
         statChart.setCreateSymbols(false);
         dynChart.setCreateSymbols(false);
+        statxyzChart.setCreateSymbols(false);
 
-        //Creating a 2 buttons button to switch between Scenes
+        //Creating a button to switch between the 2 graphs
 
-        Button scene1Switch = new Button("Dynamic");
-        Button scene2Switch = new Button("Static");
+        Button graphSwitch = new Button("Dynamic");
+        Button statxyzSwitch = new Button("Components");
 
         //Adding everything static to the static scene (scene1)
-        grid1.add(scene1Switch,1,1); //Adding the button
-        grid1.add(statChart,0, 1);
+        grid.add(graphSwitch,1,1);
+        grid.add(statxyzSwitch,2,1);
+        grid.add(statChart,0, 1);
         //Creating the static chart-scene
-        Scene scene1 = new Scene(grid1,800,600);
-
-        //Adding everything static to the dynamic scene (scene2)
-        grid2.add(scene2Switch,1,1);
-        grid2.add(dynChart,0,1);
-
-        //Creating the dynamic chart-scene
-        Scene scene2 = new Scene(grid2,800,600);
+        Scene scene1 = new Scene(grid,800,600);
 
         //Events for when pressing the buttons, scene1 -> scene2
-        scene1Switch.setOnAction(e -> {
-            //scene1Switch.setText("Static");
-            //dynSeries.setName("Resulting acceleration, dynamic");
-            stage.setScene(scene2);
+        graphSwitch.setOnAction(e -> {
+            if(graphSwitch.getText() == "Dynamic") {
+                graphSwitch.setText("Static");
+
+                grid.getChildren().remove(statChart);
+                grid.add(dynChart, 0, 1);
+                stage.setScene(scene1);
+            }
+            else{
+                graphSwitch.setText("Dynamic");
+
+                grid.getChildren().remove(dynChart);
+                grid.add(statChart,0,1);
+                stage.setScene(scene1);
+            }
         });
 
-        //scene2 -> scene1
-        scene2Switch.setOnAction(e -> {
-            stage.setScene(scene1);
+        statxyzSwitch.setOnAction(e -> {
+            if(statxyzSwitch.getText() == "Components"){
+                statxyzSwitch.setText("Resultant");
+
+                grid.getChildren().remove(statChart);
+                grid.add(statxyzChart,0,1);
+                stage.setScene(scene1);
+            }
+            else{
+                statxyzSwitch.setText("Components");
+
+                grid.getChildren().remove(statxyzChart);
+                grid.add(statChart,0,1);
+                stage.setScene(scene1);
+            }
         });
-
-
 
         //Defualt static view
 

da7895bb2de46fe0ccf237b7e62c3ae42159ef4d
gussjos <gussjos@student.chalmers.se>
2017-04-11 16:11:57 +0200

    Created table

--- src/sample/CordPan.java
+++ src/sample/CordPan.java
@@ -18,14 +18,10 @@ import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 
-/**
- * Created by gussj on 2017-04-11.
- */
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 public class CordPan extends Application {
+
+    public static final String Column1MapKey = "X";
+    public static final String Column2MapKey = "Y";
 
     public static void main(String[] args) {
         launch(args);
@@ -33,22 +29,76 @@ public class CordPan extends Application {
 
     @Override
     public void start(Stage stage) {
-        // Stage
         Scene scene = new Scene(new Group());
-        stage.setTitle("Coordinates");
+        stage.setTitle("Graphics");
         stage.setWidth(300);
         stage.setHeight(500);
 
-        final Label label = new Label("Coordiantes");
-        label.setFont(new Font("Arial", 20));
+        final Label label = new Label("Coordinates");
+        label.setFont(new Font("Arial", 30));
 
+        TableColumn<Map, String> firstDataColumn = new TableColumn<>("X");
+        TableColumn<Map, String> secondDataColumn = new TableColumn<>("Y");
+
+        // Column1MapKey refers to which data goes into which column
+        firstDataColumn.setCellValueFactory(new MapValueFactory(Column1MapKey));
+        firstDataColumn.setMinWidth(130);
+        secondDataColumn.setCellValueFactory(new MapValueFactory(Column2MapKey));
+        secondDataColumn.setMinWidth(130);
+
+        TableView table_view = new TableView<>(generateDataInMap());
+
+        table_view.setEditable(false);
+        table_view.getSelectionModel().setCellSelectionEnabled(true);
+        table_view.getColumns().setAll(firstDataColumn, secondDataColumn);
+        Callback<TableColumn<Map, String>, TableCell<Map, String>>
+                cellFactoryForMap = new Callback<TableColumn<Map, String>,
+                TableCell<Map, String>>() {
+            @Override
+            public TableCell call(TableColumn p) {
+                return new TextFieldTableCell(new StringConverter() {
+                    @Override
+                    public String toString(Object t) {
+                        return t.toString();
+                    }
+                    @Override
+                    public Object fromString(String string) {
+                        return string;
+                    }
+                });
+            }
+        };
+        firstDataColumn.setCellFactory(cellFactoryForMap);
+        secondDataColumn.setCellFactory(cellFactoryForMap);
 
         final VBox vbox = new VBox();
+
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label);
+        vbox.getChildren().addAll(label, table_view);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
         stage.setScene(scene);
+
         stage.show();
+    }
+
+    private ObservableList<Map> generateDataInMap() {
+        int max = 10;
+        ObservableList<Map> allData = FXCollections.observableArrayList();
+        for (int i = 1; i < max; i++) {
+            Map<String, String> dataRow = new HashMap<>();
+
+            String value1 = "Z" + i;
+            String value2 = "3" + i;
+            // (Associated element, value of element)
+            dataRow.put(Column1MapKey, value1);
+            dataRow.put(Column2MapKey, value2);
+            System.out.println(dataRow.get("X"));
+
+            allData.add(dataRow);
+        }
+        return allData;
     }
 }

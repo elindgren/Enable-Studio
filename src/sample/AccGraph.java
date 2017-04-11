a5d4f6cefaf4386dbbff103e8c1ff4f9e23092e2
SpaceyCat <ericlin@student.chalmers.se>
2017-04-11 14:17:56 +0200

    Created the JavaFX project

---
+++ src/sample/AccGraph.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Eric on 2017-04-11.
+ */
+public class AccGraph extends Application {
+
+    @Override public void start(Stage stage) throws Exception{
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+}
